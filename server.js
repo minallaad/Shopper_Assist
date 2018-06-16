667000b7cfac84246a4f59911b24239b38d0e534
abhinab22mohanty <abhinab22mohanty@gmail.com>
2018-06-15 23:56:05 -0700

    Server Logic refined

--- server.js
+++ server.js
@@ -5,9 +5,12 @@ var express = require('express'),
     http = require('http').Server(app),
     io = require('socket.io')(http),//.listen(server),
     //usernames format: username:socketId
-    //rooms format : connection starter ID:Array of users in room
-    usernames = {}, rooms = {};
+    //rooms format : connection starter ID:Array of Socket ID of users in room
+    //topicsassigned format: Topic name: Array of Socket ID of users
+    //r2t format: Topic name : Connection Starter ID
+    filledTopics = [],topics = ['Test0','Test1','Test2'],usernames = {}, topicassigned = {};
 
+let TOTAL_TOPICS = ['Test0','Test1','Test2'];
 var room;
 var kafka = require('no-kafka');
 const cors = require('cors');
@@ -20,15 +23,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 // let http = require('http').Server(app);
 //let io = require('socket.io')(http);
 
-var TOPIC_NAME = "Test";
-var arr = [];
-const SERVER_NAME = "A";
+
+// const SERVER_NAME = "A";
 const PORT_NUMBER = 8092;
 
 
 //Command to create topic
 
-shell.exec('/home/minal/Downloads/kafka_2.11-1.1.0/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 2 --topic ' + TOPIC_NAME);
+
 
 var bodyParser = require('body-parser')
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
@@ -42,6 +44,10 @@ app.use(cors({
 }));
 
 //Socket Connection Initialize
+TOTAL_TOPICS.forEach(topic => {
+    shell.exec('~/Softwares/kafka_2.11-1.1.0/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 2 --topic ' + topic);
+});
+
 
 io.sockets.on("connection",function(socket) {
     console.log("New Connection");
@@ -56,141 +62,224 @@ io.sockets.on("connection",function(socket) {
     });
 
     socket.on('Room request',function (data,callback) {
-        room = data.userName;
-    data.users.forEach(user => {
+        //room = data.userName;
+        let room = data.users;
+        room.push(data.userName);
+        console.log(room);
+        let topicass = null;
+        let socketArray = [];
+        //topicassiged[topicass] = [];
+    room.forEach(user => {
         // userlist.push(user);
-        if(user in usernames){
-            usernames[user].join(data.userName.toString())
-
-        }else {
-            console.log('User is not Online Or Has Left');
-            callback('User is not Online Or Has Left');
-        }
-    });
-    var TOPIC_NAME = room;
-        shell.exec('/home/minal/Downloads/kafka_2.11-1.1.0/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 2 --topic ' + TOPIC_NAME);
-    // console.log("Room");
-    //     console.log(data);
-
-
-        var producer = new kafka.Producer({
-            connectionString: 'kafka://localhost:9092',
-            clientId: 'no-kafka-client'
-        });
-
-
-
-        app.post('/postData', function (req, res) {
-            // res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1/*');
-            res.send(req.body)
-            JSON.stringify
-            var message = JSON.stringify(req.body);
-            var payloads =
+        if(topics !== null || topics!==[]) {
+            topicass = topics.pop();
+            if (user in usernames) {
+                usernames[user].join(topicass);
+                filledTopics.push(topicass);
+                socketArray.push(usernames[user]);
+                // topicassiged[topicass].push(usernames[user]);
+            }
+            else
                 {
-                    topic: TOPIC_NAME,
-                    partition: 0,
-                    message: {
-                        key: SERVER_NAME,
-                        value: message
-                    }
+                    console.log('All Topics Filled');
                 }
+
+
+            } else {
+                console.log('User is not Online Or Has Left');
+                callback('User is not Online Or Has Left');
+            }
+
+
+    });
+        topicassigned[topicass] = socketArray;
+        console.log(topicassigned);
+    // TOPIC_NAME.push(room);
+    //     shell.exec('~/Softwares/kafka_2.11-1.1.0/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 2 --topic ' + TOPIC_NAME[TOPIC_NAME.length-1]);
+    console.log("Room Job Completed");
+        console.log(data);
+
+
+
+    });
+
+
+    var producer = new kafka.Producer({
+        connectionString: 'kafka://localhost:9092',
+        clientId: 'no-kafka-client'
+    });
+
+
+    Object.keys(topicassigned).forEach(function(key) {
+        var val = topicassigned[key];
+        if(val.includes(socket)){
+             let TOPIC_NAME = key;
+            app.post('/postData', function (req, res) {
+                // res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1/*');
+                res.send(req.body)
+                // JSON.stringify
+                var message = JSON.stringify(req.body);
+                var payloads =
+                    {
+                        topic: TOPIC_NAME,
+                        partition: 0,
+                        message: {
+                            //key: SERVER_NAME,
+                            value: message
+                        }
+                    }
 
                 console.log(payloads);
 
-            return producer.init().then(function () {
-                return producer.send(payloads);
-            })
-                .then(function (result) {
-                    /*
-                    [ { topic: 'kafka-test-topic', partition: 0, offset: 353 } ]
-                    */
-                });
-            // producer.send(payloads);
-        });
-
-
-        // var dataHandler = function (messageSet, topic) {
-        //     messageSet.forEach(function (m) {
-        //         console.log(topic, m.offset, m.message.value.toString('utf8'));
-        //         //insert room io-emit logic here
-        //         io.to(room).emit('message', m.message.value.toString('utf8'));
-        //     });
-        // };
-        //
-        // function topicSet() {
-        //     var v1 = consumer.subscribe(TOPIC_NAME, 0, dataHandler);
-        //     // var  consumer.subscribe('Sample', [0, 1], dataHandler);
-        //     // var arr = []; //Creat function inside socket
-        //     arr.push([v1]);
-        //
-        //     // return arr;
-        // }
-
-
-
-        
-    });
-
-
-
-    // var kafka = require('no-kafka'),
-    //     Consumer = kafka.SimpleConsumer,
-    //     // client = new kafka.Client(),
-    //     consumer = new Consumer({
-    //         connectionString: 'kafka://localhost:9092',
-    //         clientId: 'no-kafka-client'
-    //     });//Also inside socket
-    //
-    // var dataHandler = function (messageSet, topic) {
-    //     messageSet.forEach(function (m) {
-    //         console.log(topic, m.offset, m.message.value.toString('utf8'));
-    //         //insert room io-emit logic here
-    //         io.to(room).emit('message', m.message.value.toString('utf8'));
-    //     });
-    // };//Also in same function
-    // return consumer.init().then(function () {
-    //     // Subscribe partitons 0 and 1 in a topic:
-    //     //Insert topic array loop and push logic here
-    //     return consumer.subscribe(TOPIC_NAME, 0, dataHandler);
-
-
-    http.listen(PORT_NUMBER, () => {
-        console.log('started on port ' + PORT_NUMBER);
-        var kafka = require('no-kafka'),
-            Consumer = kafka.SimpleConsumer,
-            // client = new kafka.Client(),
-            consumer = new Consumer({
-                connectionString: 'kafka://localhost:9092',
-                clientId: 'no-kafka-client'
-            });//Also inside socket
-
-        var dataHandler = function (messageSet, topic) {
-            messageSet.forEach(function (m) {
-                console.log(topic, m.offset, m.message.value.toString('utf8'));
-                //insert room io-emit logic here
-                io.to(room).emit('message', m.message.value.toString('utf8'));
+                return producer.init().then(function () {
+                    return producer.send(payloads);
+                })
+                    .then(function (result) {
+                        /*
+                        [ { topic: 'kafka-test-topic', partition: 0, offset: 353 } ]
+                        */
+                    });
+                // producer.send(payloads);
             });
-        };//Also in same function
-        return consumer.init().then(function () {
-            // Subscribe partitons 0 and 1 in a topic:
-            //Insert topic array loop and push logic here
-            var v1 = consumer.subscribe(TOPIC_NAME, 0, dataHandler);
-            // var v2= consumer.subscribe('Sample', [0, 1], dataHandler);
-            var arr = []; //Creat function inside socket
-            arr.push([v1]);//Get array and return
-            // var arr = ();
-            console.log("val:" + arr);
-            return arr;
 
-        });
-
+        }
+        else{
+            console.log('Sorry Some Error has occured');
+        }
 
     });
+
+
+
+
 
 
 
     socket.on('disconnect', function(){
-        console.log(PORT_NUMBER + " Disconnected");
+
+        Object.keys(topicassigned).forEach(function(key) {
+            var val = topicassigned[key];
+            if(val.includes(socket)){
+                var index = val.indexOf(socket);
+                if (index > -1) {
+                    val.splice(index, 1);
+                    topicassigned[key] = val;
+                }
+            }
+
+            if(topicassigned[key] === null || topicassigned[key].length){
+                delete topicassigned[key];
+
+
+                if(filledTopics.includes(key)) {
+                    var ind = filledTopics.indexOf(key);
+                    if (ind > -1) {
+                        filledTopics.splice(index, 1);
+                        topics.push(key);
+                    }
+                }
+            }
+
+
+        });
+
+
+
+        // Object.keys(rooms).forEach(function(key) {
+        //     var users = rooms[key];
+        //     if(users.includes(socket.username)){
+        //         var index = users.indexOf(socket);
+        //         if (index > -1) {
+        //             users.splice(index, 1);
+        //             rooms[key] = users;
+        //         }
+        //     }
+        // });
+
+
+
+        if(!socket.username) return;
+        delete usernames[socket.username];
+        console.log(filledTopics);
+        console.log(topics);
+        console.log(usernames);
+        console.log(rooms);
+        console.log(topicassiged);
+
+    });
+});
+// var producer = new kafka.Producer({
+//     connectionString: 'kafka://localhost:9092',
+//     clientId: 'no-kafka-client'
+// });
+//
+// app.post('/postData', function (req, res) {
+//     // res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1/*');
+//     res.send(req.body)
+//     JSON.stringify
+//     var message = JSON.stringify(req.body);
+//     var payloads =
+//         {
+//             topic: TOPIC_NAME[TOPIC_NAME.length-1],
+//             partition: 0,
+//             message: {
+//                 key: SERVER_NAME,
+//                 value: message
+//             }
+//         }
+//
+//     console.log(payloads);
+//
+//     return producer.init().then(function () {
+//         return producer.send(payloads);
+//     })
+//         .then(function (result) {
+//             /*
+//             [ { topic: 'kafka-test-topic', partition: 0, offset: 353 } ]
+//             */
+//         });
+//     // producer.send(payloads);
+// });
+
+
+
+http.listen(PORT_NUMBER, () => {
+    console.log('started on port ' + PORT_NUMBER);
+
+    var kafka = require('no-kafka'),
+        Consumer = kafka.SimpleConsumer,
+        // client = new kafka.Client(),
+        consumer = new Consumer({
+            connectionString: 'kafka://localhost:9092',
+            clientId: 'no-kafka-client'
+        });
+
+    var dataHandler = function (messageSet, TOPIC_NAME) {
+        messageSet.forEach(function (m) {
+            console.log(usernames);
+            console.log(TOPIC_NAME, m.offset, m.message.value.toString('utf8'));
+
+            io.to(TOPIC_NAME).emit('message', m.message.value.toString('utf8'));
+        });
+    };//Also in same function
+    return consumer.init().then(function () {
+        // Subscribe partitons 0 and 1 in a topic:
+        //console.log(TOPIC_NAME)
+        var arr = [];
+        TOTAL_TOPICS.forEach(topic => {
+            var v1 = consumer.subscribe(topic, 0, dataHandler);
+            // var v2= consumer.subscribe('Sample', [0, 1], dataHandler);
+
+            arr.push([v1]);
+        })
+
+
+    console.log("val:" + arr);
+    return arr;
+
+
+
+
     });
 
 
@@ -200,106 +289,3 @@ io.sockets.on("connection",function(socket) {
 
 
 
-
-
-
-//Producer
-// var producer = new kafka.Producer({
-//     connectionString: 'kafka://localhost:9092',
-//     clientId: 'no-kafka-client'
-// });
-//
-// // function postToTopicList() {
-//     app.post('/postData', function (req, res) {
-//         // res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1/*');
-//         res.send(req.body)
-//         JSON.stringify
-//         var message = JSON.stringify(req.body);
-//         var payloads =
-//             {
-//                 topic: TOPIC_NAME,
-//                 partition: 0,
-//                 message: {
-//                     key: SERVER_NAME,
-//                     value: message
-//                 }
-//             }
-//
-//         return producer.init().then(function () {
-//             return producer.send(payloads);
-//         })
-//             .then(function (result) {
-//                 /*
-//                 [ { topic: 'kafka-test-topic', partition: 0, offset: 353 } ]
-//                 */
-//             });
-//         // producer.send(payloads);
-//     });
-// }
-
-
-function postToTopicMessages() {
-    app.post('/postMessage', function (req, res) {
-        // res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1/*');
-        res.send(req.body)
-
-        var message = req.body;
-        var payloads =
-            {
-                topic: TOPIC_NAME,
-                partition: 0,
-                message: {
-                    key: SERVER_NAME,
-                    value: message.toString('utf8')
-                }
-            }
-
-        return producer.init().then(function () {
-            return producer.send(payloads);
-        })
-            .then(function (result) {
-                /*
-                [ { topic: 'kafka-test-topic', partition: 0, offset: 353 } ]
-                */
-            });
-        // producer.send(payloads);
-    });
-}
-
-//Consumer Message Adding System
-
-// function getTopicList () {
-//     http.listen(PORT_NUMBER, () => {
-//         console.log('started on port ' + PORT_NUMBER);
-//         var kafka = require('no-kafka'),
-//             Consumer = kafka.SimpleConsumer,
-//             // client = new kafka.Client(),
-//             consumer = new Consumer({
-//                 connectionString: 'kafka://localhost:9092',
-//                 clientId: 'no-kafka-client'
-//             });//Also inside socket
-//
-//         var dataHandler = function (messageSet, topic) {
-//             messageSet.forEach(function (m) {
-//                 console.log(topic, m.offset, m.message.value.toString('utf8'));
-//                 //insert room io-emit logic here
-//                 io.to(room).emit('message', m.message.value.toString('utf8'));
-//             });
-//         };//Also in same function
-//         return consumer.init().then(function () {
-//             // Subscribe partitons 0 and 1 in a topic:
-//             //Insert topic array loop and push logic here
-//             var v1 = consumer.subscribe(TOPIC_NAME, 0, dataHandler);
-//             // var v2= consumer.subscribe('Sample', [0, 1], dataHandler);
-//             var arr = []; //Creat function inside socket
-//             arr.push([v1]);//Get array and return
-//             var arr = ();
-//             console.log("val:" + arr);
-//             return arr;
-//
-//         });
-//
-//
-//     });
-
-// }
